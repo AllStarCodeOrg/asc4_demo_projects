543406404ec8bb4b49678121f0a5d191bf26b081
mahdiASC <mahdi@allstarcode.org>
2017-11-22 11:19:45 -0500

    corrected spec

--- spec/project-spec.js
+++ spec/project-spec.js
@@ -89,11 +89,11 @@ describe("Project",function(){
         });
 
         it('should throw a Type Error if the input parameter is not an array',function(){
-            expect(()=>project1.fillTeam("a string")).toThrowError(TypeError);
+            expect(()=>project1.fillTeam("a string")).toThrow();
         });
         it('should throw a Type Error if any element of the array parameter is not a Student instance',function(){
             class Student{}
-            expect(()=>project1.fillTeam([1,new Student])).toThrowError(TypeError);
+            expect(()=>project1.fillTeam([1,new Student])).toThrow();
         });
     });
 });
